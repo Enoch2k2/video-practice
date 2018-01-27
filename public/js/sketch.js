509b2a738accfd1e3a2867414eb970af62c73a71
Enoch2k2 <enoch2k2@hotmail.com>
2018-01-27 11:53:16 -0600

    added some resizing

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,21 +1,35 @@
-var video, showVideo, canvas;
-
+var video, showVideo, canvas, videoButton, muteButton, invertButton;
+var window400, window600;
+var invert = false;
 function setup() {
     canvas = createCanvas(400, 400);
-    canvas.position(10, 50);
-    var videoButton = createButton('show video');
-    videoButton.position(10, 460);
+
+    videoButton = createButton('show video');
+    videoButton.position(10, 430);
     videoButton.mousePressed(showMyVideo);
-    var muteButton = createButton('mute');
-    muteButton.position(90, 460);
+
+    muteButton = createButton('mute');
+    muteButton.position(90, 430);
     muteButton.mousePressed(mute);
+
+    invertButton = createButton('invert');
+    invertButton.position(140, 430);
+    invertButton.mousePressed(invertVideo);
+    
+    window400 = createButton('400 x 400');
+    window400.position(10, 460);
+    window400.mousePressed(resizeWindow400);
+
+    window600 = createButton('600 x 600');
+    window600.position(85, 460);
+    window600.mousePressed(resizeWindow600);
+
     var constraints = {
         video: {
             mandatory: {
                 minWidth: 1280,
                 minHeight: 720
-            },
-            optional: [{ maxFrameRate: 10 }]
+            }
         },
         audio: true
     }
@@ -34,22 +48,42 @@ function draw() {
     } else {
         background(51);
     }
+    if (invert) {
+        filter(INVERT);
+    }
 }
 
 function mute() {
     video.elt.muted = !video.elt.muted;
+    return false;
 }
 
 function showMyVideo() {
     showVideo = !showVideo;
+    return false;
 }
 
-function resizeWindow(){
-    canvas.size(mouseX, mouseY);
+function resizeWindow400(){
+    canvas.size(400, 400);
+    videoButton.position(10, 430);
+    muteButton.position(90, 430);
+    invertButton.position(140, 430);
+    window400.position(10, 460);
+    window600.position(85, 460);
+    return false;
 }
 
-function mouseDragged(){
-    canvas.size(mouseX, mouseY);
+function resizeWindow600() {
+    canvas.size(600, 600);
+    videoButton.position(10, 630);
+    muteButton.position(90, 630);
+    invertButton.position(140, 630);
+    window400.position(10, 660);
+    window600.position(85, 660);
+    return false;
+}
 
+function invertVideo(){
+    invert = !invert;
     return false;
 }
